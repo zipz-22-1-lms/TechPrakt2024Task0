0b0f4be01a34a8dc02161637dfb1b2a9d714f55c
Mykhailo Lazarchuk <zipz221_lms@student.ztu.edu.ua>
2024-05-08 15:51:25 +0300

    Create Console Project

---
+++ Practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            double x, y, result;
+            bool p;
+            Console.Write("Введіть значеня для чилса x = ");
+            if (!(double.TryParse(Console.ReadLine(), out x)))
+            {
+                do
+                {
+                    Console.WriteLine("Число введено некоректно, повторіть спробу.");
+                    Console.Write("Введіть значеня для чилса x = ");
+                    p = (double.TryParse(Console.ReadLine(), out x));
+                } while (!p);
+            }
+
+
+            Console.Write("Введіть значеня для чилса y = ");
+            if (!(double.TryParse(Console.ReadLine(), out y)))
+            {
+                do
+                {
+                    Console.WriteLine("Число введено некоректно, повторіть спробу.");
+                    Console.Write("Введіть значеня для чилса y = ");
+                    p = (double.TryParse(Console.ReadLine(), out y));
+                } while (!p);
+            }
+
+            result = x + y;
+            Console.WriteLine($"Результат обчислення: {Math.Round(result, 3)}");
+        }
+    }
+}
